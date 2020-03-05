6424b16538b82967c3f5cc0c773eeed0e3bb6aa6
Vic <t18042k@gmail.com>
2020-03-05 14:49:26 +0700

    update components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import TaskForm from './components/TaskForm'
+import Control from './components/Control'
+import TaskList from './components/TaskList'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <div className="container-fluid">
+          <div className="text-center">
+              <h1>Quản lý công việc</h1> <hr/>
+          </div>
+          <div className="row">
+              <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                {/*form*/} 
+                  <TaskForm/>
+              </div>
+              <div className="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+                <button type="button" className="btn btn-primary">
+                    <span className="fa fa-plus mr-5"></span>Thêm Công Việc
+                </button>
+
+                {/*Search-Sort*/} 
+                <Control/>
+
+                <div className="row mt-15">
+                    <TaskList/>
+                </div>
+            </div>
+          </div>
+      </div>
+    );
+  }
 }
-
 export default App;
---
+++ src/components/TaskList.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+import TaskItem from './TaskItem'
+class TaskList extends Component {
+  render(){
+    return (
+ 	<div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                    <table className="table table-bordered table-hover">
+                        <thead>
+                            <tr>
+                                <th className="text-center">STT</th>
+                                <th className="text-center">Tên</th>
+                                <th className="text-center">Trạng Thái</th>
+                                <th className="text-center">Hành Động</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td></td>
+                                <td>
+                                    <input type="text" className="form-control" />
+                                </td>
+                                <td>
+                                    <select className="form-control">
+                                        <option value="-1">Tất Cả</option>
+                                        <option value="0">Ẩn</option>
+                                        <option value="1">Kích Hoạt</option>
+                                    </select>
+                                </td>
+                                <td></td>
+                            </tr>
+                            <TaskItem/>
+                        </tbody>
+                    </table>
+        </div>
+    );
+  }
+}
+export default TaskList;
